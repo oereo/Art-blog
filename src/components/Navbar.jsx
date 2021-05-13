30266de567a987b604c4df76a521b4091e47c0ee
oereo <dlstpgns0406@gmail.com>
2021-05-14 03:29:53 +0900

    refactor : add hover effect in navbar and fix format

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,66 +1,67 @@
 import React from 'react';
+import MenuIcon from "../assets/images/menu_icon.jpg"
+import styled from "styled-components";
 
-// let CSSTransitionGroup = React.addons.CSSTransitionGroup;
+const StyledDoor = styled.span`
+    display: flex;
+    height: 120px;
+    margin:0 50px 0 0;
+    width: 70px;
+    float: right;
+    
+    &:hover {
+    opacity : 0.4;
+    }
+`
 
 class Submenu extends React.Component {
-  render() {
-    return (
-      <ul className="nav__submenu">
-        <li className="nav__submenu-item ">
-          <a>Our Company</a>
-        </li>
-        <li className="nav__submenu-item ">
-          <a>Our Team</a>
-        </li>
-        <li className="nav__submenu-item ">
-          <a>Our Portfolio</a>
-        </li>
-      </ul>
-    )
-  }
+    render() {
+        return (
+            <ul className="nav__submenu" style={{float:"right", margin: "120px -95px 0 0"}}>
+                <li className="nav__submenu-item " style={{listStyle:"none"}}>
+                    <a>Chamber Project</a>
+                </li>
+
+            </ul>
+        )
+    }
 }
 
 class Menu extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showAboutMenu: false
+    constructor(props) {
+        super(props);
+        this.state = {
+            showAboutMenu: false
+        };
+    }
+
+    handleHover = (event) => {
+        this.setState({showAboutMenu: true});
     };
-  }
 
-  handleHover = (event) => {
-    this.setState({ showAboutMenu: true });
-  };
+    handleLeave = (event) => {
+        this.setState({showAboutMenu: false});
+    };
 
-  handleLeave = (event) => {
-    this.setState({ showAboutMenu: false });
-  };
+    render() {
+        return (
+            <nav className="nav">
+                <ul className="nav__menu" style={{listStyle:"none"}}>
+                    <li className="nav__menu-item" onMouseLeave={this.handleLeave} style={{listStyle:"none", paddingLeft:"0px"}}>
+                        <a onMouseEnter={this.handleHover}>
+                            <StyledDoor>
+                                <img style={{width: "100%", height: "100%"}} src={MenuIcon} alt="menu"/>
+                            </StyledDoor>
+                        </a>
+                        <div className="submenu-container">
+                            {this.state.showAboutMenu && <Submenu/>}
+                        </div>
+                    </li>
 
-  render() {
-    return (
-      <nav className="nav">
-        <ul className="nav__menu">
-          <li className="nav__menu-item">
-            <a>Home</a>
-          </li>
-          <li className="nav__menu-item" onMouseLeave={this.handleLeave}>
-            <a onMouseEnter={this.handleHover}>
-              About
-            </a>
-            <div className="submenu-container">
-
-                { this.state.showAboutMenu && <Submenu /> }
-
-            </div>
-          </li>
-
-          <li className="nav__menu-item">
-            <a>Contact</a>
-          </li>
-        </ul>
-      </nav>
-    )
-  }
+                </ul>
+            </nav>
+        )
+    }
 }
 
 export default Menu;
