1af69bb5d1e075f609c41fe03abb1d3158a4a2d4
oereo <dlstpgns0406@gmail.com>
2021-05-15 11:46:44 +0900

    refactor: fix statement

--- src/components/SubContent.jsx
+++ src/components/SubContent.jsx
@@ -32,45 +32,48 @@ const ProjectDescription = styled.div`
 const SubContent = () => (
     <>
         <ProjectDescription>
-            A room of one's own indicates one's mind. Have you ever heard of the voice of objects when you slide into
-            someone else's personal space? The space and the object are organically formed that become a gate to the
-            world of cohabitant, having a role of interface. Chamber Project enable us to open the gate of rooms of
+            A room of one's own indicates one's mind. <br/>
+            Have you ever heard of the voice of objects when you slide into
+            someone else's personal space? <br/>
+            The space and the object are organically formed that become a gate to the
+            world of cohabitant, having a role of interface.<br/><br/>
+            Chamber Project enable us to open the gate of rooms of
             imaginary ones regardless of space and time, through the 3D version. The chamber signifies '-sil' which
             means a room for specific purpose, '-sil' which means a room in terms of cardiac things at once. Each
             chamber is drawing its breath as chambers of heart do. Unlike the room of reality, we would spend our time
             more innerly and clandestinely in the virtual 3D space.
         </ProjectDescription>
 
-        <Grid container spacing={3} style={{margin: "100px 0 0 5%", display:"flex"}}>
+        <Grid container spacing={3} style={{margin: "100px 0 0 5%", display: "flex"}}>
             <Grid item xs={12} style={{margin: "20% 0 0 0"}}>
                 <br/>
             </Grid>
-            <Grid item lg={3} md={4} sm={6} xs={12}>
+            <Grid item lg={4} md={4} sm={6} xs={12}>
                 <img
                     style={{
-                        width: "120%",
-                        height: "120%",
-                        margin:"0 0 0 0",
-                        objectFit:"fill"
+                        width: "100%",
+                        height: "100%",
+                        margin: "0 0 0 0",
+                        objectFit: "fill"
                     }}
                     src={firstImg} alt="im"/>
             </Grid>
-            <Grid item lg={3} md={4} sm={6} xs={12}>
+            <Grid item lg={4} md={4} sm={6} xs={12}>
                 <img
                     style={{
-                        width: "120%",
-                        height: "120%",
-                        margin: "0 0 0 150px",
-                        objectFit:"cover"
+                        width: "100%",
+                        height: "100%",
+                        margin: "0 0 0 20px",
+                        objectFit: "fill"
                     }}
                     src={secondImg} alt="im"/>
             </Grid>
             <Grid item lg={3} md={4} sm={6} xs={12} direction="column"
-                  // align="left"
+                // align="left"
                   style={{
                       // display: "flex",
-                      padding: "50px 0 0 0",
-                      margin: "200px 0 0 240px",
+                      padding: "45% 0 0 0",
+                      margin: "0 0 0 40px",
                   }}>
                 {"<"}Fog{">"}<br/>
                 Dahee Kwon, Cloud oil on canvas, 2021
@@ -81,12 +84,11 @@ const SubContent = () => (
             <Grid item lg={3} md={4} sm={6} xs={12}>
 
             </Grid>
-            <Grid item lg={3} md={4} sm={6} xs={12}>
+            <Grid item lg={3} md={4} sm={6} xs={12} style={{padding: "5% 0 0 0"}}>
                 <img
                     style={{
-                        width: "110%",
-                        height: "90%",
-                        margin: "20% 0 0 0"
+                        width: "100%",
+                        height: "100%",
                     }}
                     src={thirdImg} alt="im"/>
                 <div style={{textAlign: "center"}}>
@@ -95,11 +97,11 @@ const SubContent = () => (
                 </div>
 
             </Grid>
-            <Grid item xs={4}>
+            <Grid item lg={3} xs={4}>
                 <img
                     style={{
-                        width: "110%",
-                        height: "110%",
+                        width: "120%",
+                        height: "120%",
                         margin: "0 0 0 10%"
                     }}
                     src={fourthImg} alt="im"/>
@@ -109,12 +111,12 @@ const SubContent = () => (
             </Grid>
             <Grid item xs={1}>
             </Grid>
-            <Grid item xs={4}>
+            <Grid item lg={5} xs={5}>
                 <img
                     style={{
                         width: "100%",
                         height: "100%",
-                        margin: "20% 0 0 0"
+                        margin: "0% 0 0 0"
                     }}
                     src={fifthImg} alt="im"/>
                 {"<"}Untitled{">"}<br/>
@@ -123,28 +125,29 @@ const SubContent = () => (
             </Grid>
             <Grid item xs={1}>
             </Grid>
-            <Grid item xs={5}>
-                <Grid item xs={6}>
+            <Grid item lg={5} xs={5} style={{padding: "0 0 0 0"}}>
+                <Grid item lg={6} xs={6}>
                     <img
                         style={{
-                            width: "140%",
+                            width: "120%",
                             height: "120%",
-                            margin: "0 0 0 20%",
-                            padding: "10% 0 0 0 "
+                            margin: "-20% 0 0 15%",
+                            padding: "-10% 0 0 0 "
                         }}
                         src={sixthImg} alt="im"/>
                 </Grid>
-                <Grid item xs={6}>
+                <Grid item lg={6} xs={6}>
                     <img
                         style={{
-                            width: "130%",
-                            height: "120%",
-                            margin: "15% 0 0 80%"
+                            width: "100%",
+                            height: "100%",
+                            margin: "0 0 0 60%",
+                            padding: "70% 0 0 0"
                         }}
                         src={seventhImg} alt="im"/>
                 </Grid>
             </Grid>
-            <Grid item xs={12} style={{margin: "40% 0 0 0"}}>
+            <Grid item mg={12} lg={12} style={{margin: "40% 0 0 0"}}>
                 <br/>
             </Grid>
             <Grid item xs={4}>
@@ -169,7 +172,7 @@ const SubContent = () => (
                 <img
                     style={{
                         width: "100%",
-                        height: "90%",
+                        height: "100%",
                         margin: "0 0 0 0"
                     }}
                     src={ninthImg} alt="im"/>
@@ -283,25 +286,29 @@ const SubContent = () => (
             </Grid>
         </Grid>
         <ProjectDescription>
-Nest of a hummingbird is consist of cotton, moss and web, differentiated from any other nests. These enigmatic materials make them more flexible and infrangible. She is captivated by that distinctive identity. A gleam of the nest of hummingbird looks like an amethyst in her eyes. Every night, she observes the body of her own covered with tiny water drops in the tub. Close her eyes, Imagine a white circle. What can we see inside the circle? She opens her eyes again, stares her body. The Blue body.
-<br/>
-            <br/>
-A Private Bond.<br/>
-Dream flows from the outside of sleep.
+            Nest of a hummingbird is consist of cotton, moss and web, differentiated from any other nests. These
+            enigmatic materials make them more flexible and infrangible. She is captivated by that distinctive identity.
+            A gleam of the nest of hummingbird looks like an amethyst in her eyes. <br/><br/>
+            Every night, she observes the body of
+            her own covered with tiny water drops in the tub. Close her eyes, Imagine a white circle. What can we see
+            inside the circle? She opens her eyes again, stares her body. The Blue body.
+            <br/><br/>
+            A Private Bond.<br/>
+            Dream flows from the outside of sleep.
         </ProjectDescription>
         <Grid container spacing={3} style={{margin: "100px 0 0 0"}}>
             <Grid item xs={6} style={{margin: "10% 0 0 0"}}>
                 <br/>
             </Grid>
             <Grid item xs={4} style={{margin: "10% 0 0 0"}}>
-                <Link to = "/email">
-                <img
-                    style={{
-                        width: "70%",
-                        height: "70%",
-                        margin: "0"
-                    }}
-                    src={antImg} alt="im"/></Link>
+                <Link to="/email">
+                    <img
+                        style={{
+                            width: "70%",
+                            height: "70%",
+                            margin: "0"
+                        }}
+                        src={antImg} alt="im"/></Link>
             </Grid>
         </Grid>
     </>
